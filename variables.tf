03aa72898779e2f561b7b33cbcb3e2266e79ca4f
GivenCingco <givencingco@gmail.com>
2024-09-19 18:06:29 +0200

    Added defaults in variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 variable "dockerhub_cred" {
   type = string
+  default = "arn:aws:secretsmanager:us-east-1:009160050878:secret:codebuild/dockerhub-bh0WqA"
   description = "Variable for DockerHub credentials"
 }
 
 variable "codestart_connector_cred" {
   type = string
-    description = "Variable for CodeStar connection credentials"
+  default = "arn:aws:codeconnections:us-east-1:009160050878:connection/0ea54640-35fd-4696-a116-e6b9b0623370"
+  description = "Variable for CodeStar connection credentials"
 
 }
