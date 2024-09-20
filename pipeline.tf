0f3d4eacb39fa97a49ec6c1d0b556bef20ac2788
GivenCingco <givencingco@gmail.com>
2024-09-20 17:38:53 +0200

    Added nodejs 18 image to test stage

--- pipeline.tf
+++ pipeline.tf
@@ -34,7 +34,7 @@ resource "aws_codebuild_project" "test_stage" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
+    image                       = "public.ecr.aws/sam/build-nodejs18.x:latest"
     type                        = "LINUX_CONTAINER"
   }
 
