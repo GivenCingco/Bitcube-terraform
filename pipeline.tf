bfea7a8b663cb27989d0eb7ee815bb00b0b13db7
GivenCingco <givencingco@gmail.com>
2024-09-21 11:22:30 +0200

    Added the nextjs zip files location

--- pipeline.tf
+++ pipeline.tf
@@ -120,8 +120,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
       configuration = {
         ApplicationName     = aws_codedeploy_app.app.name
         DeploymentGroupName = aws_codedeploy_deployment_group.deployment_group.deployment_group_name
-        S3Bucket = module.s3_bucket.s3_bucket_id 
-        S3Key    = "my-nextjs-app.zip" 
+        S3Location = "${module.s3_bucket.s3_bucket_id}/my-nextjs-app.zip" 
       }
     }
   }
